d5e3db0866fb07a7235805a769a2a10745290100
stu26757451251 <stu26757451251@gmail.com>
2026-04-05 13:43:32 +0800

    test: refactor server tests for lifecycle management and update math utility test cases

--- backend/test/server.test.ts
+++ backend/test/server.test.ts
@@ -2,10 +2,14 @@ import { describe, expect, test } from 'vitest'
 import { serverOf } from '../src/server'
 
 describe('Server Testing', () => {
-  const server = serverOf()
+  // BeforeAll
+  // AfterAll
 
-  test('When send a GET request to /ping, it should return status code 200', async () => {
-    // act: send a GET request to /ping
+  test('Given a running server, When receive a GET /ping request, Then it should response with status code 200', async () => {
+    // arrange: a running server
+    const server = serverOf()
+
+    // act: receive a GET /ping request
     const response = await server.inject({
       method: 'GET',
       url: '/ping'
--- backend/test/utils.test.ts
+++ backend/test/utils.test.ts
@@ -1,33 +1,36 @@
 import { describe, expect, it } from 'vitest'
 import { myCustomAdd, fabonacci } from '../src/utils/math'
-import { fail } from 'assert'
 
 describe('utils testing', () => {
   describe('add function', () => {
     it('should return 3 when add 1 and 2', () => {
+      // arrange
+      const a = 1
+      const b = 2
+
       // act
-      const result = myCustomAdd(1, 2)
+      const result = myCustomAdd(a, b)
 
       // assert
       expect(result).toBe(3)
     })
     it('should return 5 when add 2 and 3', () => {
-      // TODO: fix the test
-      fail('not implemented')
+      expect(myCustomAdd(2, 3)).toBe(5)
     })
   })
 
   describe('fabonacci function', () => {
     it('should return 1 when n is 1', () => {
-      expect(fabonacci(1)).toBe(1)
+      // TODO: fix the test
+      expect.fail('not implemented')
     })
     it('should return 1 when n is 2', () => {
       // TODO: fix the test
-      fail('not implemented')
+      expect.fail('not implemented')
     })
     it('should return 2 when n is 3', () => {
       // TODO: fix the test
-      fail('not implemented')
+      expect.fail('not implemented')
     })
   })
 })
